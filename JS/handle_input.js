50c8316d6cd887b731c497b7c44f4bf7759c9fab
alb-ctrl <61526801+alb-ctrl@users.noreply.github.com>
2021-04-06 10:49:57 -0400

    enabled AJAX call
    program now uses AJAX to call our .cgi file and gets the respond back

--- JS/handle_input.js
+++ JS/handle_input.js
@@ -1,22 +1,33 @@
 $(document).ready(function() {
    $('#input_form').on("submit", function(event){  
   event.preventDefault();  
-  if($('#inpf').val() != "")  {  
+
+  var inpf = $('#inpf').val();
+  var outf = $('#outf').val();
    $.ajax({  
-    url:"handler.cgi",  
+    url:"../cgi-bin/handler.cgi",  
     method:"POST",  
     data:$('#input_form').serialize(),  
     success:
-    function(data){  
-    alert(data);  
-    // $('#home_page').html(data); 
+    function(data){
+    if (inpf != "") {
+    	$('#outf').val(data); 
+    }
+    else if (outf != "") {
+    	$('#inpf').val(data); 
+    }
+    //alert(data);  
+    //$('#test').html(data); 
     },
     error: function(xhr, status, error){
          var errorMessage = xhr.status + ': ' + xhr.statusText
          alert('Error - ' + errorMessage);
      }  
-   });  
-  }  
+   }).done(function(data){
+    console.log(data);
+    
+}); 
+  
  });
 
 });
